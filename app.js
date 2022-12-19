db5665f00471f27549a7e141670f64810a6af497
prajjawal tiwari <kingindnorth.0311@gmail.com>
2022-12-19 16:42:18 +0530

    express server

---
+++ app.js
@@ -0,0 +1,10 @@
+const express = require("express")
+require("dotenv").config()
+
+const app = express()
+
+const PORT = process.env.PORT || 5000
+
+app.listen(PORT,()=>{
+    console.log(`server started on port ${PORT}`)
+})
