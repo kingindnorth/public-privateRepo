df9370f40009f098c8db30e16716bc2433cfb5bf
prajjawal tiwari <kingindnorth.0311@gmail.com>
2022-12-19 16:54:05 +0530

    db config

--- app.js
+++ app.js
@@ -1,4 +1,6 @@
 const express = require("express")
+const connect = require("./utils/db")
+
 require("dotenv").config()
 
 const app = express()
@@ -6,5 +8,6 @@ const app = express()
 const PORT = process.env.PORT || 5000
 
 app.listen(PORT,()=>{
+    connect()
     console.log(`server started on port ${PORT}`)
 })
