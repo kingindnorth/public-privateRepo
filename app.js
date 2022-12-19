8f0949377029b177f780298976b323178d356269
prajjawal tiwari <kingindnorth.0311@gmail.com>
2022-12-19 17:02:58 +0530

    views handlbars

--- app.js
+++ app.js
@@ -1,9 +1,22 @@
 const express = require("express")
+const {engine} = require("express-handlebars")
 const connect = require("./utils/db")
 
 require("dotenv").config()
 
 const app = express()
+
+//set view engine
+app.engine("handlebars",engine())
+app.set("view engine","handlebars")
+
+//serving static files
+app.use(express.static("public"))
+
+//body-parser-json
+app.use(express.json())
+//body-parser-form
+app.use(express.urlencoded({extended:true}))
 
 const PORT = process.env.PORT || 5000
 
