72f960e415bbff3c36c449375aebbe3800c739d2
prajjawal tiwari <kingindnorth.0311@gmail.com>
2022-12-21 17:09:36 +0530

    code fix

---
+++ controllers/strategy.js
@@ -0,0 +1,25 @@
+const User = require("../models/User")
+
+const strategy = async(accessToken, refreshToken, profile, done)=>{
+    const newUser = {
+        googleId:profile.id,
+        firstname:profile.name.givenName,
+        lastname:profile.name.familyName,
+        photo:profile.photos[0].value
+    }
+    try{
+        const user = await User.findOne({googleId:profile.id})
+        if(user){
+            done(null,user)
+        }
+        else{
+            user = await User.create(newUser)
+            done(null,user)
+        }
+    }catch(err){
+        console.log(err)
+    }
+    
+}
+
+module.exports = strategy
--- utils/strategy.js
+++ utils/strategy.js
@@ -1,4 +1,5 @@
 const GoogleStrategy = require("passport-google-oauth20").Strategy
+const strategy = require("../controllers/strategy")
 const User = require("../models/User")
 
 const googleStrategy = (passport) => {
@@ -7,27 +8,7 @@ const googleStrategy = (passport) => {
             clientID: process.env.GOOGLE_CLIENT_ID,
             clientSecret: process.env.GOOGLE_CLIENT_SECRET,
             callbackURL: process.env.CALLBACK_URL
-        },async(accessToken, refreshToken, profile, done)=>{
-            const newUser = {
-                googleId:profile.id,
-                firstname:profile.name.givenName,
-                lastname:profile.name.familyName,
-                photo:profile.photos[0].value
-            }
-            try{
-                const user = await User.findOne({googleId:profile.id})
-                if(user){
-                    done(null,user)
-                }
-                else{
-                    user = await User.create(newUser)
-                    done(null,user)
-                }
-            }catch(err){
-                console.log(err)
-            }
-            
-        })
+        },strategy)
     )
     passport.serializeUser((user, done) => {
         done(null, user.id)
