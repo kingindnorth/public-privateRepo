ea7591eed945d5efa20e398e7d4f613b2792b6d5
prajjawal tiwari <kingindnorth.0311@gmail.com>
2022-12-21 16:31:43 +0530

    layout added

--- routes/index.js
+++ routes/index.js
@@ -3,10 +3,14 @@ const router = require("express").Router()
 const {verifyAuth, verifyLogin} = require("../middlewares/auth")
 
 router.get("/",verifyLogin,(req,res)=>{
-    res.render("index")
+    res.render("index",{
+        layout:"index"
+    })
 })
 
 router.get("/dashboard",verifyAuth,(req,res)=>{
-    res.render("dashboard")
+    res.render("dashboard",{
+        name:req.user.firstname,
+    })
 })
 module.exports = router
