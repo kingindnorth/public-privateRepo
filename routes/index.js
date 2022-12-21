e972cac1881ace80c7684d4e6698982f2ec212fc
prajjawal tiwari <kingindnorth.0311@gmail.com>
2022-12-21 16:51:18 +0530

    layout changes

--- routes/index.js
+++ routes/index.js
@@ -4,13 +4,15 @@ const {verifyAuth, verifyLogin} = require("../middlewares/auth")
 
 router.get("/",verifyLogin,(req,res)=>{
     res.render("index",{
-        layout:"index"
+        isAuthenticated:req.isAuthenticated()
     })
 })
 
 router.get("/dashboard",verifyAuth,(req,res)=>{
     res.render("dashboard",{
         name:req.user.firstname,
+        isAuthenticated:req.isAuthenticated()
+
     })
 })
 module.exports = router
